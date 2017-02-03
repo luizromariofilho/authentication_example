35e91849b33ab94cfdae9bd4e185bb60df81d259
Luiz Romário Lopes de Sant'Ana Filho <Luiz Romrio Lopes de Sant'Ana Filho>
2017-02-02 23:13:45 -0300

    Create form and css

--- web/js/app.js
+++ web/js/app.js
@@ -1,6 +1,46 @@
-angular.module('app', ['ng-token-auth'])
-	.config(function($authProvider) {
-        $authProvider.configure({
-            apiUrl: 'http://localhost:3000/'
+var app = angular.module('app', ['ng-token-auth', 'ui.router']);
+
+app.config(function($authProvider) {
+	$authProvider.configure({
+		apiUrl: 'http://localhost:3000'
+	});
+});
+
+app.config(function($stateProvider,$urlRouterProvider) {
+  	$urlRouterProvider.otherwise("/hello");
+
+	var helloState = {
+		name: 'hello',
+		url: '/hello',
+		templateUrl: 'views/hello.html'
+	}
+
+	var aboutState = {
+		name: 'about',
+		url: '/about',
+		templateUrl: 'views/about.html'
+	}
+
+	var loginState = {
+		name: 'login',
+		url: '/login',
+		templateUrl: 'views/login.html'
+	}
+
+	$stateProvider.state(helloState);
+	$stateProvider.state(aboutState);
+	$stateProvider.state(loginState);
+});
+
+
+app.controller('IndexCtrl', function($scope, $auth) {
+    $scope.handleLoginBtnClick = function() {
+      $auth.submitLogin($scope.loginForm)
+        .then(function(resp) {
+          console.log('funcionou');
+        })
+        .catch(function(resp) {
+          console.log('nao funcionou');
         });
-    });
+    };
+  });
